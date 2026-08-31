e50c4b972cb8baa63896ab8b65ac84b8f1a4925c
amarnath25scs1003004906 <amarnath.25scs1003004906@iilm.edu>
2026-08-31 23:04:26 +0530

    Add files via upload

---
+++ Book.h
@@ -0,0 +1,29 @@
+
+#ifndef BOOK_H
+#define BOOK_H
+
+#include <string>
+using namespace std;
+
+class Book {
+private:
+    int id;
+    string title;
+    string author;
+    bool available;
+
+public:
+    Book();
+    Book(int id, string title, string author, bool available = true);
+
+    int getId() const;
+    string getTitle() const;
+    string getAuthor() const;
+    bool isAvailable() const;
+
+    void setTitle(string title);
+    void setAuthor(string author);
+    void setAvailable(bool available);
+};
+
+#endif
